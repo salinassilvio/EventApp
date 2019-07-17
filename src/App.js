198e100988972e5d42eaca8b83343bcd5af010cc
Silvio Salinas <ssalinas.silvio@gmail.com>
2019-07-16 23:30:36 -0600

    Creando componente Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-   <h1>HOla</h1>
+   <Fragment>
+     <Header></Header>
+   </Fragment>
   );
 }
 
