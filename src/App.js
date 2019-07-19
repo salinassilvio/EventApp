d8e4727f86b39dbd7447b145641c9387ab8a7dfe
Silvio Salinas <ssalinas.silvio@gmail.com>
2019-07-18 23:59:31 -0600

    Se termina context y consumir el api con axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import Header from './components/Header';
 
+import CategoriasProvider from './context/CategoriasContext'
+
 function App() {
   return (
-   <Fragment>
+   <CategoriasProvider>
      <Header></Header>
-   </Fragment>
+   </CategoriasProvider>
   );
 }
 
--- src/context/CategoriasContext.js
+++ src/context/CategoriasContext.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 //Crear el context
 const CategoriasContext = React.createContext();
@@ -6,17 +7,32 @@ export const CategoriasConsumer = CategoriasContext.Consumer;
 
 class CategoriasProvider extends Component {
    
-   token = '64EVETHEP6IDOFUB7D';
-    state = {  }
+   token = 'WRU5O3GFAAAW4G2LU4VI';
+    state = { 
+        categorias : []
+     }
 
     componentDidMount(){
         this.obtnerCategorias();
     }
-    obtnerCategorias = ()=> {
-        let url = ``
+    obtnerCategorias = async ()=> {
+        let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`
+        let categorias = await axios.get(url);
+        //console.log(categorias.data.categories);
+        this.setState({
+            categorias : categorias.data.categories
+        })
     }
     render() { 
-        return (  );
+        return ( 
+            <CategoriasContext.Provider
+                value ={{
+                    categorias :  this.state.gateorias
+                }}
+            >
+                {this.props.children}
+            </CategoriasContext.Provider>
+         );
     }
 }
  
